801804db2e6dedf5e90c076002a132b847cd1cb5
Artsiom Rusak <artsiom.rusak@outlook.com>
2022-07-31 21:25:26 +0300

    Update.

--- src/HttpTriggerTest2/index.ts
+++ src/HttpTriggerTest2/index.ts
@@ -5,7 +5,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully. From HttpTriggerTest2." + customState.name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully. From HttpTriggerTest2.." + customState.name
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
